59f5ac16900af47aa48f4b8319eac38cdfb49e1c
rama <ramirogbazan@gmail.com>
2025-10-13 19:52:36 -0300

    home

--- aplications/home/views.py
+++ aplications/home/views.py
@@ -3,7 +3,7 @@ from django.views.generic import TemplateView
 # Create your views here.
 
 class IndexView(TemplateView) :
-    template_name = 'base_home.html'
+    template_name = 'home.html'
 
 class AboutView(TemplateView) :
     template_name = 'about/about.html'
